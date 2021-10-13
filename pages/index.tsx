9427b94e8b5ccfe0ae0f2ab95edcc5ad0562d0a4
Nanai10a <Nanai10a@outlook.jp>
2021-10-13 14:54:55 +0900

    🚧 separate components.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,45 @@
-import React from "https://esm.sh/react@17.0.2";
+import React, { FC } from "https://esm.sh/react@17.0.2";
 
 export default function Page() {
   return (
     <body>
+      <Header />
+      <Main />
+      <Footer />
+    </body>
+  );
+}
+
+const Main: FC<None> = () => {
+  return (
+    <>
+      <main>
+        Contents
+      </main>
+    </>
+  );
+};
+
+type None = Record<string, never>;
+
+const Header: FC<None> = () => {
+  return (
+    <>
       <header>
         <nav>
           Title
         </nav>
       </header>
-      <main>
-        Contents
-      </main>
+    </>
+  );
+};
+
+const Footer: FC<None> = () => {
+  return (
+    <>
       <footer>
         Footer
       </footer>
-    </body>
+    </>
   );
-}
+};
