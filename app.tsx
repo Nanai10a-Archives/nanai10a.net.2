f79b93f14cdb0d1affd43f512f0423aa8c572cb4
Nanai10a <Nanai10a@outlook.jp>
2021-10-27 08:14:18 +0900

    🚧 add font: `Roboto` (from gstatic).

--- app.tsx
+++ app.tsx
@@ -7,6 +7,17 @@ export default function App(
     <main>
       <head>
         <meta name="viewport" content="width=device-width" />
+        {/* Roboto link */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <Page {...pageProps} />
     </main>
